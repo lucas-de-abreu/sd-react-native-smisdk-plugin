0b933da97bcb7dc1b5c0af5b52993010dee90e7d
Pawan Bawankar <pawan.bawankar@datami.com>
2022-02-22 14:42:44 +0530

    TS-1704, Do not override integration if Application class not available or readable.

--- smisdk_integration.js
+++ smisdk_integration.js
@@ -145,21 +145,22 @@ function projectConfigAndroid(folder) {
     // Application class not available. 
     //Copy application class and update manifest with application name
 
-    const manifest = readManifest(manifestPath);
-    const packageName = getPackageName(manifest);
-    const packageNameStr = "package " + packageName + ';\n';
-    const packageFolder = packageName.replace(/\./g, path.sep);
-    const appPackagePath = path.join(sourceDir, `src/main/java/${packageFolder}`);
+    console.log('Application class not available. Please use sample ApplicaitionClass.txt to create one and update AndroidManifest.xml with application name.';
+    // const manifest = readManifest(manifestPath);
+    // const packageName = getPackageName(manifest);
+    // const packageNameStr = "package " + packageName + ';\n';
+    // const packageFolder = packageName.replace(/\./g, path.sep);
+    // const appPackagePath = path.join(sourceDir, `src/main/java/${packageFolder}`);
 
-    var datamiAppFile = fs.readFileSync('ApplicationClass.txt', 'utf8')
-    if (datamiAppFile.search(packageNameStr) < 0) {
-      var datamiAppFile = insert(datamiAppFile, 0, packageNameStr);
-    }
-    var datamiApplicationClassName = 'MainApplication';
-    fs.writeFileSync(appPackagePath + '/' + datamiApplicationClassName + '.java', datamiAppFile, 'utf8');
+    // var datamiAppFile = fs.readFileSync('ApplicationClass.txt', 'utf8')
+    // if (datamiAppFile.search(packageNameStr) < 0) {
+    //   var datamiAppFile = insert(datamiAppFile, 0, packageNameStr);
+    // }
+    // var datamiApplicationClassName = 'MainApplication';
+    // fs.writeFileSync(appPackagePath + '/' + datamiApplicationClassName + '.java', datamiAppFile, 'utf8');
 
     // update manifest with app name
-    updateManifestFile(manifestPath, datamiApplicationClassName);
+    // //updateManifestFile(manifestPath, datamiApplicationClassName);
 
     // update configuration file
     const stringsXmlPath = findStringsXml(sourceDir);
